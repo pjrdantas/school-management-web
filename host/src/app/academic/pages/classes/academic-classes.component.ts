77bdd2d2ee3d21754d239e9bf6c436d955dab077
Paulo Dantas <20131563+pjrdantas@users.noreply.github.com>
2026-04-26 00:57:41 -0300

    Corrige lock de histórico mantendo URL atual do router

---
+++ host/src/app/academic/pages/classes/academic-classes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academic-classes',
+  standalone: true,
+  templateUrl: './academic-classes.component.html',
+  styleUrls: ['./academic-classes.component.scss'],
+})
+export class AcademicClassesComponent {}
